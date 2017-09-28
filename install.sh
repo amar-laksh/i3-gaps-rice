6a44a689fcbe7f1359a76515275c2e8aab9e1b04
TechRunner2 <techrunner2.0@gmail.com>
2017-09-27 23:11:47 -0500

    Altered a few things

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mv -r .* $HOME/
-sudo pacman -S --noconfirm compton rofi blender krita w3m firefox youtube-dl 
-packer -S --noconfirm i3-gaps youtube-dl-gui-git oh-my-zsh-git
+sudo pacman -S --noconfirm compton rofi blender krita w3m firefox youtube-dl pavucontrol rxvt-unicode rxvt-unicode-terminfo busybox blueman bluez cherrytree cronie cpupower htop powertop cups networkmanager dnsmasq exfat-utils feh ffmpeg flac freetype2
+packer -S --noconfirm i3-gaps youtube-dl-gui-git oh-my-zsh-git cli-visualizer
